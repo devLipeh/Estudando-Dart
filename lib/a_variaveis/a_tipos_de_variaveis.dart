78dc683a0447cae60d2cc844b8bd797d5cb6a4ce
Filipe Santos <filipe.santos0330@gmail.com>
2022-06-22 22:09:14 -0300

    conhecendo variáveis

---
+++ lib/a_variaveis/a_tipos_de_variaveis.dart
@@ -0,0 +1,23 @@
+/*
+  Tipos:
+    String -> usado para palavras
+    Numbers:
+      int -> usado para números inteiros
+      double -> usado para números com ponto flutuantes
+    bool -> variável de controle que pode ser verdadeira ou falsa
+
+  Forma de declarar:
+    tipo nome_variavel; ou tipo nome_variavel = valor_correto;
+*/
+
+void main() {
+  String name = "Filipe";
+  int age = 20;
+  double height = 1.90;
+  bool programming = true;
+
+  print("Meu nome é $name");
+  print("Minha idade é $age");
+  print("Eu tenho $height de altura");
+  print("E eu estou programando? $programming");
+}
