a811dad3ba8663a1b66e1bb3e55714e1b0699ad7
Filipe Santos <classicmagic404@gmail.com>
2022-06-23 23:06:40 -0300

    condições multiplas

---
+++ lib/b_condicao/c_condicao.dart
@@ -0,0 +1,15 @@
+/*
+  Condições multiplas são para fazer mais de uma condição em uma única estrutura
+*/
+
+void main() {
+  String work = "developer";
+
+  if (work == "artist") {
+    print("Você deve ter ótimas artes!");
+  } else if (work == "developer") {
+    print("Vê se não esquece os ; em sksk");
+  } else {
+    print("Ainda não sei o que falar a respeito desse trabalho :(");
+  }
+}
