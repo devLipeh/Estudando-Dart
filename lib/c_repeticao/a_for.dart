076a67f9dd0ad7ef2399a5edb62b13eb7550c3b8
Filipe Santos <classicmagic404@gmail.com>
2022-06-24 00:23:34 -0300

    estrutura de repetição - fo

---
+++ lib/c_repeticao/a_for.dart
@@ -0,0 +1,17 @@
+/*
+  Em Dart estrutura de repetições são usadas para repetir n vezes um bloco de código.
+  Essa estrutura pode ser feita de 3 formas sendo elas o "for", "while" e o "do while"
+  Nesse exemplo usei o for que têm a seguinte estrutura:
+    for(inicialização, condição, incrementação) {
+      bloco de código
+    }
+*/
+
+void main() {
+  String name = "Filipe";
+  List<String> lastNames = ["José", "Hunter", "Santos"];
+
+  for (int i = 0; i < lastNames.length; i++) {
+    print("opção ${i + 1}º - $name ${lastNames[i]}");
+  }
+}
