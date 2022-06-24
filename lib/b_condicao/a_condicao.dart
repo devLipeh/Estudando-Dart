c4de8adfc33bb5cdde605fc932b86ea03ce28bad
Filipe Santos <classicmagic404@gmail.com>
2022-06-23 22:17:18 -0300

    Conhecendo estruturas de condição

---
+++ lib/b_condicao/a_condicao.dart
@@ -0,0 +1,11 @@
+/*
+  Em Dart condicoes servem para fazer um certo controle se uma coisa deve ou não ser execultada.
+*/
+
+void main() {
+  bool adult = true;
+
+  if (adult == true) {
+    print("Você já está de maior");
+  }
+}
