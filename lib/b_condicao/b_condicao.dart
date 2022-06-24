838f6880228920abdfea01ecaa48808b213e1462
Filipe Santos <classicmagic404@gmail.com>
2022-06-23 22:48:36 -0300

    conhecendo o else

---
+++ lib/b_condicao/b_condicao.dart
@@ -0,0 +1,12 @@
+/*
+  A key word "else" serve para executar um trecho de código como uma exceção a uma condição
+*/
+
+void main() {
+  bool adult = false;
+  if (adult == true) {
+    print("Você já é de maior :)");
+  } else {
+    print("Você ainda não é de maior :(");
+  }
+}
