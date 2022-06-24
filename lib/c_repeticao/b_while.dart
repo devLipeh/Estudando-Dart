c395c8e42d1d8552c68cf5d3bc335cbc8694ca24
Filipe Santos <classicmagic404@gmail.com>
2022-06-24 00:32:29 -0300

    estrutura da repetição - while

---
+++ lib/c_repeticao/b_while.dart
@@ -0,0 +1,13 @@
+/*
+  Diferente da estrutura de repetição for o while recebe apenas
+  uma condição que será usada para determinar a quantidade de vezes que o bloco será executado
+*/
+
+void main() {
+  int num = 1;
+
+  while (num <= 10) {
+    print(num);
+    num++;
+  }
+}
