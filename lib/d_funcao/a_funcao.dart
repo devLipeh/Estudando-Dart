65f5513c5106344067545d9f05eced41dee6557d
Filipe Santos <classicmagic404@gmail.com>
2022-06-24 11:31:20 -0300

    Entendendo funções

--- lib/d_funcao/a_funcao.dart
+++ lib/d_funcao/a_funcao.dart
@@ -2,6 +2,8 @@
   Funções em Dart são usados quando queremos isolar um bloco de código
   e só chama-lo em um momento especifico, as funções podem ou não
   receber parâmetros e retornar valores.
+
+  Nesse exemplo fiz uma função básica sem parâmetros e sem retorno
 */
 
 void main() {
