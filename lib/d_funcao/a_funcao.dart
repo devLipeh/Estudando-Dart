1d849e7459a54f45d46a297a28a990e8c9c2947f
Filipe Santos <classicmagic404@gmail.com>
2022-06-24 11:29:36 -0300

    funções com retorno

---
+++ lib/d_funcao/a_funcao.dart
@@ -0,0 +1,14 @@
+/*
+  Funções em Dart são usados quando queremos isolar um bloco de código
+  e só chama-lo em um momento especifico, as funções podem ou não
+  receber parâmetros e retornar valores.
+*/
+
+void main() {
+  helloWorld();
+}
+
+// Função sem retorno e sem parâmetros
+void helloWorld() {
+  print("Hello World!");
+}
---
+++ lib/d_funcao/b_retornos.dart
@@ -0,0 +1,18 @@
+/*
+  Funções com soma podem retornar qualquer tipo como por exemplo:
+
+    void -> função sem retorno
+    int -> retorno de números inteiros
+    double -> retorno de números com ponto flutuante
+    String -> retorno de palavras ou letras
+    List -> retorno de lista
+    ... e assim por diante
+*/
+
+void main() {
+  print(soma());
+}
+
+int soma() {
+  return 1 + 2;
+}
